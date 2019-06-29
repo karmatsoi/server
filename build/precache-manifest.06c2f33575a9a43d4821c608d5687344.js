da4c43e589fe26a073f22cb534897b55753a63ed
karmatsoi <karmatsoi@gmail.com>
2019-06-30 00:01:58 +0800

    add build

---
+++ build/precache-manifest.06c2f33575a9a43d4821c608d5687344.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e5cc4bd8c133751454b81e15500efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "592d6983395d4cb7375b",
+    "url": "/static/js/main.a745c80c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/static/js/main.a745c80c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},9:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),a=t(2);var c=function(){return r.a.createElement("div",null,"Hello")};t.n(a).a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.a745c80c.chunk.js.map
