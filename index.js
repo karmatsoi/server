64d368ab22ebe090111dd0a4d2b949330e5057f3
karmatsoi <karmatsoi@gmail.com>
2019-06-29 22:01:14 +0800

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello!!!')
+})
+
+app.listen(process.env.PORT)
